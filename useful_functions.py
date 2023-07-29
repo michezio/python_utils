d299d78ee8510c5edeaf2e03ce691c0478621392
Michele Abruzzese <33634641+michezio@users.noreply.github.com>
2023-07-29 10:59:54 +0200

    [useful_functions] added find_best_arrangement

--- useful_functions.py
+++ useful_functions.py
@@ -111,3 +111,24 @@ def extract_info_from_file_path(path:str):
     *name, extension = file_name.split('.')
     name = '.'.join(name)
     return parent, file_name, name, extension
+
+
+def find_best_arrangement(N):
+    best_wasted_space = float('inf')
+    best_width = 1
+    best_height = N
+
+    for width in range(1, N // 2):
+        height = (N + width - 1) // width
+
+        if width > height:
+            break
+
+        wasted_space = (width * height) - N
+
+        if wasted_space < best_wasted_space or (wasted_space == best_wasted_space and abs(width - height) < abs(best_width - best_height)):
+                best_wasted_space = wasted_space
+                best_width = width
+                best_height = height
+
+    return best_width, best_height
